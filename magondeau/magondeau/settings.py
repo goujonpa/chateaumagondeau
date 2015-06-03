9b4c7ac70a1f459dcdc2295348bc4c848fbb816f
Paul GOUJON <Polo@vpnetu1-127.utc>
2015-06-03 19:03:48 -0300

    creating blocks in base template

--- magondeau/magondeau/settings.py
+++ magondeau/magondeau/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'magondeau.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,10 +70,6 @@ TEMPLATES = [
         },
     },
 ]
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'templates'),
-)
 
 WSGI_APPLICATION = 'magondeau.wsgi.application'
 
